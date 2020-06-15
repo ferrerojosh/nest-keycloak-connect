0a631440daf26c3ee7e8fa02b8a42995a9ae8645
John Joshua Ferrer <johnjoshuaferrer@disroot.org>
2020-06-16 02:25:17 +0800

    Update docs

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
+/**
+ * Used internally, provides keycloak options for the Nest guards.
+ */
 export const KEYCLOAK_CONNECT_OPTIONS = 'KEYCLOAK_CONNECT_OPTIONS';
 
+/**
+ * Key for injecting a keycloak instance.
+ */
 export const KEYCLOAK_INSTANCE = 'KEYCLOAK_INSTANCE';
--- src/decorators/unprotected.decorator.ts
+++ src/decorators/unprotected.decorator.ts
@@ -4,10 +4,12 @@ export const META_UNPROTECTED = 'unprotected';
 
 /**
  * Allow user to use unprotected routes.
+ * @since 1.2.0
  */
 export const Unprotected = () => SetMetadata(META_UNPROTECTED, true);
 
 /**
  * Alias for `@Unprotected`.
+ * @since 1.2.0
  */ 
 export const Public = () => SetMetadata(META_UNPROTECTED, true);
