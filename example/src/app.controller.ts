0041a36fb73de2b22114b2eb5af8a74e4664414f
John Joshua Ferrer <johnjoshuaferrer@disroot.org>
2021-04-12 22:20:58 +0800

    Add authenticated user decorator, closes #43

--- example/src/app.controller.ts
+++ example/src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { AuthenticatedUser } from 'nest-keycloak-connect';
 
 @Controller()
 export class AppController {
-
   @Get()
-  getHello(): string {
-    return "Hello world!";
+  getHello(
+    @AuthenticatedUser()
+    user: any,
+  ): string {
+    if (user) {
+      return `Hello ${user.preferred_username}`;
+    } else {
+      return 'Hello world!';
+    }
   }
 }
