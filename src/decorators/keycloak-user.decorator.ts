8b47749589119efd4a341074884dafd26247b97e
John Joshua Ferrer <johnjoshuaferrer@disroot.org>
2024-10-19 07:03:31 +0800

    Fix wrong imports

--- src/decorators/keycloak-user.decorator.ts
+++ src/decorators/keycloak-user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { extractRequest } from '../util';
+import { extractRequest } from '../internal.util';
 
 /**
  * Retrieves the current Keycloak logged-in user.
