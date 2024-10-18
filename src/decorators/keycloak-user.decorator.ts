d55cf07436c05a19e53497f6e7f2b3f57490856b
John Joshua Ferrer <johnjoshuaferrer@disroot.org>
2024-10-19 03:00:59 +0800

    Rename @AuthenticatedUser to @KeycloakUser

--- src/decorators/keycloak-user.decorator.ts
+++ src/decorators/keycloak-user.decorator.ts
@@ -5,7 +5,7 @@ import { extractRequest } from '../util';
  * Retrieves the current Keycloak logged-in user.
  * @since 1.5.0
  */
-export const AuthenticatedUser = createParamDecorator(
+export const KeycloakUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const [req] = extractRequest(ctx);
     return req.user;
